a4d483ba30d6416a89502707f0b1337504776ef7
Aleksandr <tabruka@yandex.ru>
2022-12-01 19:54:35 +0300

    Добавил код задачи

---
+++ task/Program.cs
@@ -0,0 +1,31 @@
+﻿string[] array = new string[] {"hello", "2", "world", ":)"};
+string[] newArray = FindLessThan(array, 3);
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", newArray)}]");
+
+string[] FindLessThan(string[] input, int n) 
+{
+    string[] output = new string[CountLessThan(input, n)];
+    for(int i = 0, j = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= n) 
+        {
+            output[j] = input[i];
+            j = j + 1;
+        }
+    }
+    return output;
+}
+
+int CountLessThan(string[] input, int n) 
+{
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= n) 
+        {
+            count = count + 1;
+        }
+    }
+    return count;
+}
